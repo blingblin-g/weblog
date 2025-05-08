df1d131557913f809ec208903f1be32825dfdff3
blingblin-g <luvbliny@gmail.com>
2025-05-08 22:52:58 +0900

    Add initial Django project structure with Wagtail integration

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--@t*6d#*w%t0@3o#-q27c*ftm_&x)d%jn9=gc38sb%)bzy9y5x"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
