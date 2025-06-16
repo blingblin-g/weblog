293296fdd6c1704fdcc5b358738ade80b9eb5aa4
blingblin-g <luvbliny@gmail.com>
2025-06-16 19:53:05 +0900

    Remove unused configuration files and update Docker and manage.py settings for new project structure

--- weblog/settings/dev.py
+++ weblog/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--@t*6d#*w%t0@3o#-q27c*ftm_&x)d%jn9=gc38sb%)bzy9y5x"
+SECRET_KEY = "django-insecure-r-=-crjui7r1n&3p_l$7dbjalk!l=!jzw#3()h!6&p(#h(h-u#"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
