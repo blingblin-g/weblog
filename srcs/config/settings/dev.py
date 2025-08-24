f8d067bc725fefd91aa9ab019c4aff970543c03c
blingblin-g <luvbliny@gmail.com>
2025-08-24 15:36:01 +0900

    Update environment configuration in Docker setup; add .env.production and .env files for production and development environments, respectively. Modify Django settings to use environment variables for sensitive configurations and enable dynamic debugging based on environment.

--- srcs/config/settings/dev.py
+++ srcs/config/settings/dev.py
@@ -1,15 +1,15 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '1') == '1'
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r&d+tij1-*5f(qol9*0l3m$8(!$##aqgge!7v87bzw)tft_#r6"
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-r&d+tij1-*5f(qol9*0l3m$8(!$##aqgge!7v87bzw)tft_#r6")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', "django.core.mail.backends.console.EmailBackend")
 
 
 try:
